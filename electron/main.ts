2642e8191e76bcf5e653bfe9695c8707dc287ac7
Rudy <my4my4@gmail.com>
2026-06-14 20:21:11 +0900

    피아노 교육 프로그램 제작 중

---
+++ electron/main.ts
@@ -0,0 +1,40 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+import path from 'path'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+    titleBarStyle: 'hiddenInset',
+    backgroundColor: '#0f0f0f',
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173')
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.handle('get-app-version', () => app.getVersion())
